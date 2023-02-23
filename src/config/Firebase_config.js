f79f07a651f2c0081ef66330329a66f8e10dd615
mantas pc <mantas.gulbinskas@gmail.com>
2023-02-23 12:29:42 +0200

    config firebase connection

---
+++ src/config/Firebase_config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+const FirebaseConfig = {
+    apiKey: "AIzaSyBAc4NR4P3xLHtWfmck1Qvf_k-kgqftEIE",
+    authDomain: "todo-list-124.firebaseapp.com",
+    projectId: "todo-list-124",
+    storageBucket: "todo-list-124.appspot.com",
+    messagingSenderId: "193852773582",
+    appId: "1:193852773582:web:fa741289d64dbff96f6c9f",
+    measurementId: "G-SFSCXSXMVN"
+};
+
+// Initialize Firebase
+const app = initializeApp(FirebaseConfig);
+    const auth = getAuth(app);
+
+export { auth };
+export default FirebaseConfig;
