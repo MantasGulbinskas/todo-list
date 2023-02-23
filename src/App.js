4eed82274a633eef7a8777a03e3e1ecdaafb08d9
mantas pc <mantas.gulbinskas@gmail.com>
2023-02-23 12:35:51 +0200

    modified files, add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {Routes, Route} from "react-router-dom";
+import {SingIn} from "./components/SignIn";
+import {Home} from "./pages/Home";
+import {SignUp} from "./components/SignUp";
+import {Dashboard} from "./pages/Dashboard";
+import {AuthContextProvider} from "./context/AuthContext";
+import {ProtectedRoute} from "./components/ProtectedRoute";
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div>
+<AuthContextProvider>
+    <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/log-in' element={<SingIn />} />
+        <Route path='/dashboard'  element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+    </Routes>
+</AuthContextProvider>
+        </div>
+
   );
 }
 
