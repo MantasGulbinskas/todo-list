510d2c766d317d8cd7b621bc9859bd4210bd93bf
mantas pc <mantas.gulbinskas@gmail.com>
2023-02-24 23:40:58 +0200

    modified sign in if user already connect, then dont show sign in page and redirect directly to dashboard

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,14 @@
 import React, {useState} from "react";
-import {Link, useNavigate} from 'react-router-dom';
+import {Link, Navigate, useNavigate} from 'react-router-dom';
 import {UserAuth} from "../context/AuthContext";
+
 
 export const SingIn = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
     const [error, setError] = useState('')
-    const {signIn} = UserAuth();
     const navigate = useNavigate()
-
+    const {signIn, user} = UserAuth();
 
     const logIn = async () => {
         try {
@@ -18,36 +18,40 @@ export const SingIn = () => {
             setError(e.message)
         }
     }
-    return (
-        <section>
-            <h1>{error}</h1>
-            <div className="container mt-5 m w-25">
-                <div className="col justify-content-center">
-                    <div className="d-flex flex-row align-items-center mb-4">
-                        <div className="form-outline flex-fill mb-0">
-                            <input type="text" placeholder='Email' className='form-control' value={email}
-                                   onChange={(e) => setEmail(e.target.value)}/>
+    if (!user) {
+        return (
+            <section>
+                <h1>{error}</h1>
+                <div className="container mt-5 m w-25">
+                    <div className="col justify-content-center">
+                        <div className="d-flex flex-row align-items-center mb-4">
+                            <div className="form-outline flex-fill mb-0">
+                                <input type="text" placeholder='Email' className='form-control' value={email}
+                                       onChange={(e) => setEmail(e.target.value)}/>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="d-flex flex-row align-items-center mb-4">
-                        <div className="form-outline flex-fill mb-0">
-                            <input type="password" placeholder='password' className='form-control' value={password}
-                                   onChange={(e) => setPassword(e.target.value)}/>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                            <div className="form-outline flex-fill mb-0">
+                                <input type="password" placeholder='password' className='form-control' value={password}
+                                       onChange={(e) => setPassword(e.target.value)}/>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="d-flex justify-content-center mx-4 -mb-3 mb-lg-4">
-                        <button onClick={logIn} className="btn btn-primary btn-lg">Log in</button>
-                    </div>
-                    <div>
-                        <p>Not register? <a href="/sign-up">Sign up</a></p>
+                        <div className="d-flex justify-content-center mx-4 -mb-3 mb-lg-4">
+                            <button onClick={logIn} className="btn btn-primary btn-lg">Log in</button>
+                        </div>
+                        <div>
+                            <p>Not register? <a href="/sign-up">Sign up</a></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div><h2><Link to='/'>Home</Link></h2></div>
-        </section>
-    )
+                <div><h2><Link to='/'>Home</Link></h2></div>
+            </section>
+        )
+    }else {
+        return <Navigate to='/dashboard' />
+    }
 }
 
 
