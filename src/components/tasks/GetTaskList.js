cc4e0ca421c39432da98c051dab33fc65131096c
mantas pc <mantas.gulbinskas@gmail.com>
2023-03-01 09:35:39 +0200

    create task list, get data from db and show in table

---
+++ src/components/tasks/GetTaskList.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react';
+import {collection, query, orderBy, onSnapshot} from 'firebase/firestore'
+import {db} from "../../config/Firebase_config";
+import {Link} from "react-router-dom";
+
+export const GetTaskList = () => {
+  const [tasks, setTasks] = useState([])
+
+
+  useEffect(() => {
+    const q = query(collection(db, 'tasks'), orderBy('createdAt', 'desc'))
+    onSnapshot(q, (querySnapshot) => {
+      setTasks(querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data(),
+        date: doc.data().createdAt.toDate().toDateString(),
+        time: doc.data().createdAt.toDate().toLocaleTimeString()
+      })))
+    })
+  },[])
+  return (<div className='container'>
+    <Link to='/tasks/add'> <button className="btn btn-primary">Add task +</button></Link>
+
+      <table className="table text-capitalize">
+        <thead>
+        <tr>
+          <th scope='col'>title</th>
+          <th scope='col'>description</th>
+          <th scope='col'>create Time</th>
+          <th scope='col'>created by</th>
+        </tr>
+        </thead>
+          <tbody>
+      {tasks.map((task) => (
+          <tr key={task.id}>
+            <th scope='row'>{task.data.title}</th>
+            <th>{task.data.description}</th>
+            <th>{task.date} {task.time}</th>
+            <th>{task.data.creator}</th>
+
+          </tr>
+      ))}
+          </tbody>
+    </table>
+      </div>
+  );
+}
