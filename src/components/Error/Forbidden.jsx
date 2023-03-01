2ce8e5fe9e0c75263edfeff6cc6a24e4641d30a4
mantas pc <mantas.gulbinskas@gmail.com>
2023-03-01 09:31:10 +0200

    creating error component and style

---
+++ src/components/Error/Forbidden.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/Forbidden.css'
+import {Link} from "react-router-dom";
+export const Forbidden = () => {
+
+  return (
+      <section className='background'>
+        <div className='text container'>
+        <h1 className='text-danger'>Sorry folks, page is forbidden.</h1>
+        <p>The moose out front shoulda told ya.</p>
+           <p>You must go to <Link to='/sign-up'>Sign Up</Link> Or <Link to='/log-in'>Sign in</Link></p>
+
+    </div>
+      </section>
+  );
+}
