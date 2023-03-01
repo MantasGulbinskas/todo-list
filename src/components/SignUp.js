faf8a2014b674f88ab2be2117e3bdcd1190dec49
Windows-PC <mantas.gulbinskas@gmail.com>
2023-03-01 17:35:13 +0200

    change useState to useReducer

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,62 +1,149 @@
-
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import {UserAuth} from "../context/AuthContext";
 
+function registerReducer(state, action) {
+    if (action.type === "input") {
+        return {
+            ...state,
+            [action.name]: action.value
+        };
+    } else if (action.type === "submit") {
+        return {
+            ...state,
+            error: null,
+            loading: true
+        };
+    } else if (action.type === "success") {
+        return {
+            ...state,
+            registered: true,
+            loading: false,
+            error: null
+        };
+    } else if (action.type === "error") {
+        return {
+            ...state,
+            loading: false,
+            error: action.error
+        };
+    }
+}
+
+const initialReducerValue = {
+    loading: false,
+    error: null,
+    name: "",
+    lastname: "",
+    username: "",
+    password: "",
+    email: "",
+    registered: false
+};
+
 export const SignUp = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const [username, setUsername] = useState('')
+
     const {createUser} = UserAuth()
     const navigate = useNavigate()
 
+    const [state, dispatch] = useReducer(registerReducer, initialReducerValue);
+    const validateInputs = (inputs) => {
+
+        for (const inputName of Object.values(inputs)) {
+            const inputValue = inputs[inputName]
+            if (inputValue === '') {
+                dispatch({type: 'error', error: `${inputName} is required`})
+                return false
+            }
+        }
+        return true;
+    }
+    const areInputsFilled = validateInputs(state)
     const signUp = async (e) => {
         e.preventDefault()
         try {
-         await createUser(email,password, username)
-            navigate('/dashboard')
-        } catch (e) {
-            setError(e.message)
+            if (areInputsFilled) {
+                await createUser(
+                    state.email,
+                    state.password,
+                    state.username,
+                    state.name,
+                    state.lastname
+                )
+                    .then(() => dispatch({type: 'success'}))
+                navigate('/dashboard')
+
+            }
+        } catch (error) {
+            dispatch({type: 'error', error: error.code});
         }
     };
+
+
     return (
-        <section>
-            <div className="container mt-5 m w-25">
-                <h1>{error}</h1>
-
-                <div className="col justify-content-center">
-                    <div className="d-flex flex-row align-items-center mb-4">
-                        <div className="form-outline flex-fill mb-0">
-                            <input type="text" placeholder='username' className='form-control' value={username}
-                                   onChange={(e) => setUsername(e.target.value)}/>
-                        </div>
-                    </div>
-                </div>
-                <div className="col justify-content-center">
-                    <div className="d-flex flex-row align-items-center mb-4">
-                        <div className="form-outline flex-fill mb-0">
-                            <input type="text" placeholder='Email' className='form-control' value={email}
-                                   onChange={(e) => setEmail(e.target.value)}/>
-                        </div>
-                    </div>
-
-                    <div className="d-flex flex-row align-items-center mb-4">
-                        <div className="form-outline flex-fill mb-0">
-                            <input type="password" placeholder='password' className='form-control' value={password}
-                                   onChange={(e) => setPassword(e.target.value)}/>
-                        </div>
-                    </div>
-
-                    <div className="d-flex justify-content-center mx-4 -mb-3 mb-lg-4">
-                        <button onClick={signUp} className="btn btn-primary btn-lg">Register</button>
-                    </div>
-                    <div>
-                        <p>Already have account? <a href="/log-in">Log in</a></p>
-                    </div>
-                </div>
+        <section style={{maxWidth: '400px'}} className='text-center p-5 m-auto w-100'>
+            <h1>Register</h1>
+            <p>{state.error && state.error}</p>
+            <div className="form-floating">
+                <input type="email" name='email' required className='form-control' id='email'
+                       placeholder='name@example.com'
+                       value={state.email} onChange={e =>
+                    dispatch({
+                        type: 'input',
+                        name: e.target.name,
+                        value: e.target.value
+                    })}/>
+                <label htmlFor="email" className='fs-4'>Email address</label>
+                {state.email === '' && <span className="text-danger">Email is required</span>}
             </div>
-            <div><h2><Link to='/'>Home</Link></h2></div>
+            <div className="form-floating">
+                <input type="text" name='username' className='form-control' id='username' value={state.username}
+                       onChange={e =>
+                           dispatch({
+                               type: 'input',
+                               name: e.target.name,
+                               value: e.target.value
+                           })}/>
+                <label htmlFor="username" className='fs-4'>Username</label>
+                {state.username === '' && <span className="text-danger">username is required</span>}
+            </div>
+            <div className="form-floating">
+                <input type="text" className='form-control' name='name' id='name' placeholder='name@example.com'
+                       value={state.name} onChange={e =>
+                    dispatch({
+                        type: 'input',
+                        name: e.target.name,
+                        value: e.target.value
+                    })}/>
+                <label htmlFor="name" className='fs-4'>Name</label>
+                {state.name === '' && <span className="text-danger">Name is required</span>}
+            </div>
+            <div className="form-floating">
+                <input type="text" className='form-control' name='lastname' id='lastname' placeholder='name@example.com'
+                       value={state.lastname} onChange={e =>
+                    dispatch({
+                        type: 'input',
+                        name: e.target.name,
+                        value: e.target.value
+                    })}/>
+                <label htmlFor="lastname" className='fs-4'>Last Name</label>
+                {state.lastname === '' && <span className="text-danger">Last name is required</span>}
+            </div>
+            <div className="form-floating">
+                <input type="password" name='password' className='form-control' id='floatingPassword'
+                       placeholder='Password' value={state.password} onChange={e =>
+                    dispatch({
+                        type: 'input',
+                        name: e.target.name,
+                        value: e.target.value
+                    })}/>
+                <label htmlFor="floatingPassword" className='fs-4'>Password</label>
+                {state.password === '' && <span className="text-danger">Password is required</span>}
+
+            </div>
+            <button onClick={signUp} className="w-100 btn btn-lg btn-primary my-5" type='submit'>Sign up</button>
+            <p>Already have account? <Link to='/log-in'>Log in</Link></p>
+
         </section>
     )
 }
