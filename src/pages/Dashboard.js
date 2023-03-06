9eeb0872226d7f0a3908d361c44acfb3e4b67b71
Windows-PC <mantas.gulbinskas@gmail.com>
2023-03-06 13:57:42 +0200

    Renamed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,41 +1,14 @@
-// import React, {useEffect, useState} from "react";
-// import {UserAuth} from "../context/AuthContext";
-// import {db} from "../config/Firebase_config";
-// import {doc, getDoc} from 'firebase/firestore'
 import Sidebar from "../components/sidebar/Sidebar";
 import Navbar from "../components/navbar/Navbar";
-import './dashboard.scss'
 
 export const Dashboard = () => {
-    // const {user} = UserAuth();
-    //
-    // const [username, setUsername] = useState('')
-    // const [error, setError] = useState('')
-
-    // useEffect(() => {
-    //     const fetchInfo = async () => {
-    //         try {
-    //             const data = user.uid;
-    //             const docRef = doc(db, 'users_information', data)
-    //             const docSnap = await getDoc(docRef);
-    //             const userInformation = docSnap.data();
-    //             setUsername(userInformation)
-    //         } catch (e) {
-    //             setError(e.message)
-    //             setTimeout(() => {
-    //                 setError('')
-    //             }, 2000)
-    //         }
-    //     }
-    //     fetchInfo();
-    // }, [user]);
-    //
 
     return (
-        <div className="home ">
+        <div className="d-flex m-auto">
                 <Sidebar/>
-            <div className="homeContainer ">
+            <div style={{flex: 12}} >
             <Navbar/>
+                <h1>Hello!</h1>
             </div>
         </div>
     )
