3ce9a74e13d4805f48b46ba61294edfca65fa816
Windows-PC <mantas.gulbinskas@gmail.com>
2023-03-10 19:09:51 +0200

    Fix: modified navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,43 +1,23 @@
-import "./navbar.scss";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
-import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
-import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import {useLocalStorage} from "../../hooks/UseLocalStorage";
+import React from "react";
 
 const Navbar = () => {
-
+    const [dataStorage] = useLocalStorage()
     return (
-        <div className="navbar">
-            <div className="wrapper row">
-                <div className="search position-relative col-10 col-sm-4 col-xl-2 my-sm-0">
-                    <input type="text" placeholder="Search..." />
-                    <SearchOutlinedIcon className='position-absolute end-0 top-0 my-1'/>
+        <nav style={{height: '68px'}} className="border-bottom  ">
+            <div className="d-flex align-items-center justify-content-between">
+                <div className=" col-8 col-sm-4 col-xl-2 ms-3">
+                    <input className='col-10' type="text" placeholder="Search..."/>
                 </div>
-                <div className="items row-cols-12 col-sm-3 mt-4 my-sm-0">
+                <img style={{width: '60px', height: '50px'}}
+                     src={dataStorage.photoURL}
+                     alt="profile_photo"
+                     className="rounded-circle mt-2 me-3"
+                />
 
-                    <div className="item">
-                        <LanguageOutlinedIcon className="icon" />
-                        English
-                    </div>
-
-                    <div className="item">
-                        <NotificationsNoneOutlinedIcon className="icon" />
-                        <div className="counter">1</div>
-                    </div>
-
-                    <div className="item">
-                        <ListOutlinedIcon className="icon" />
-                    </div>
-                    <div className="item">
-                        <img
-                            src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-                            alt=""
-                            className="avatar"
-                        />
-                    </div>
-                </div>
             </div>
-        </div>
+
+        </nav>
     );
 };
 
